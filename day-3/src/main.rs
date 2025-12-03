eecfbba3aac5cafe4394f0caa9a3ad941485aa59
Stefan Mermans <stefanmermans99@gmail.com>
2025-12-03 21:00:23 +0100

    day 3 part 2

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -12,33 +12,44 @@ struct Args {
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
+    let mut batteries_to_turn_on: usize = 2;
+    
+    if args.second_part {
+        batteries_to_turn_on = 12;
+    }
+
     let total_joltage = fs::read_to_string(args.input)?
         .split('\n')
         .filter(|bank|!bank.is_empty())
-        .map(|bank|get_joltage(bank).unwrap())
-        .sum::<i32>();
+        .map(|bank|get_joltage(bank, batteries_to_turn_on).unwrap())
+        .sum::<i64>();
 
     println!("Total joltage: {} jolts", total_joltage);
     Ok(())
 }
 
-fn get_joltage(bank: &str) -> Result<i32, Box<dyn Error>> {
-    print!("{}", bank);
+fn get_joltage(bank: &str, remaining: usize) -> Result<i64, Box<dyn Error>> {
+    let mut result = String::new();
+    let mut remaining = remaining;
+    let mut offset = 0;
+    
+    while remaining > 0 {
+        let slice = bank[offset..(bank.len() - remaining + 1)].to_string();
+        let slice_result = get_largest_value(slice);
+        result.push(slice_result.1);
+        offset = offset + slice_result.0 + 1;
+        
+        remaining -= 1;
+    }
 
-    let (index, largest_start) = get_largest_value(bank[0..(bank.len()-1)].to_string());
-    let (_, largest_end) = get_largest_value(bank[(index+1)..=(bank.len()-1)].to_string());
-    let result = format!("{}{}", largest_start, largest_end);
-
-    println!(" = {}", result);
-
-    return Ok(result.parse::<i32>()?);
+    return Ok(result.parse::<i64>()?);
 }
 
-fn get_largest_value(bank: String) -> (usize, char) {
+fn get_largest_value(bank_slice: String) -> (usize, char) {
     let mut largest: char = '0';
     let mut largest_index = 0;
 
-    for (index, char) in bank.char_indices() {
+    for (index, char) in bank_slice.char_indices() {
         if char > largest {
             largest = char;
             largest_index = index;
